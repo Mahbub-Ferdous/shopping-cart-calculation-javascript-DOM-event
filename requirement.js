e0f8597cedc97ab97e7b37cdaad480ef3ea2c53f
Mahbub-Ferdous <mahbubferdous14@gmail.com>
2022-05-15 16:05:11 +0600

    some changes done

---
+++ requirement.js
@@ -0,0 +1,10 @@
+// 1. increase and decrease product quantity by using +, - icon with javascript
+// 2. if we increse product quantity the price will be multiple by quantity and show there
+// 3. if we also decrease product quantity the price will be decrease
+// 4. all products price additon one another and than show in sub total
+// 5. add the tax parcentage with subtotal and show total price
+
+// first we build a function called 'getInputFieldValue()'
+// second we build a function called 'quantityIncreaseDecrease()' for quantity increase and decrease by using one function and pass two parameter
+// thired we build a function called 'priceIncreaseDecrease()' for price field update and this will be use both products depend on quantity field increase and decrease
+// fourth we build a function called 'calculateTotal()' this for subtotal,tax and total field update function for increse quantity and decrease quantity and both products
